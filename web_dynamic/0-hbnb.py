787be8756cc3a103a34f527bb8ca8a90566e11b5
Alexus91 <mrfadlkamal@gmail.com>
2023-11-05 14:36:01 -0800

    readme update and  0. Last clone!

--- web_dynamic/0-hbnb.py
+++ web_dynamic/0-hbnb.py
@@ -20,7 +20,24 @@ def close_db(error):
 
 @app.route('/0-hbnb/', strict_slashes=False)
 def hbnb():
-    return render_template('0-hbnb.html', cache_id=str(uuid.uuid4()))
+    """ HBNB is alive! """
+    states = storage.all(State).values()
+    states = sorted(states, key=lambda k: k.name)
+    st_ct = []
+
+    for state in states:
+        st_ct.append([state, sorted(state.cities, key=lambda k: k.name)])
+
+    amenities = storage.all(Amenity).values()
+    amenities = sorted(amenities, key=lambda k: k.name)
+
+    places = storage.all(Place).values()
+    places = sorted(places, key=lambda k: k.name)
+
+    return render_template('0-hbnb.html',
+                           states=st_ct,
+                           amenities=amenities,
+                           places=places)
 
 
 if __name__ == "__main__":
